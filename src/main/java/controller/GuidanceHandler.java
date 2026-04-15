b06bdba495c8217af073dc14d0d77e7b5f294433
Lyro7 <lovro.lupis7@gmail.com>
2026-04-15 16:26:02 +0200

    Added loop and fixed controller lifecycle

--- src/main/java/controller/GuidanceHandler.java
+++ src/main/java/controller/GuidanceHandler.java
@@ -1,15 +1,56 @@
 package controller;
 
+import javafx.animation.AnimationTimer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GuidanceHandler {
 
     private MainController mainController;
+
+    private final List<Controller> guidanceControllers = new ArrayList<>();
+
+    private AnimationTimer animator;
+
+    public GuidanceHandler() {
+        navigationLoop();
+    }
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
 
     public void switchToTab(String fileName) {
-        mainController.switchTabs(fileName);
+        mainController.switchContentOfTab(fileName);
+    }
+
+    public void navigationLoop() {
+        animator = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                //TODO
+            }
+        };
+    }
+
+    public void startNavigationLoop() {
+        animator.start();
+    }
+
+    public void stopNavigationLoop() {
+        animator.stop();
+    }
+
+    public void resetControllers() {
+        for (Controller controller : guidanceControllers) {
+            controller.close();
+        }
+        guidanceControllers.clear();
+    }
+
+    public void addGuidanceController(Controller controller) {
+        this.guidanceControllers.add(controller);
     }
 
 }
