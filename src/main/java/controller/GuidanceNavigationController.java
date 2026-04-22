2cd180f6d0ed7b535662f981c10eb3652ac18911
Lyro7 <lovro.lupis7@gmail.com>
2026-04-22 02:59:28 +0200

    Added fxml nodes to controller

--- src/main/java/controller/GuidanceNavigationController.java
+++ src/main/java/controller/GuidanceNavigationController.java
@@ -3,11 +3,24 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class GuidanceNavigationController implements Controller {
+public class GuidanceNavigationController implements GuidanceController {
+
+    @FXML
+    public Circle tLight1;
+    @FXML
+    public Circle tLight2;
+    @FXML
+    public Circle tLight3;
+    @FXML
+    public VBox depthBox;
+    @FXML
+    public Label depth;
 
     private GuidanceHandler guidanceHandler;
 
@@ -16,17 +29,17 @@ public class GuidanceNavigationController implements Controller {
 
     @Override
     public void registerController() {
-        Controller.super.registerController();
+        GuidanceController.super.registerController();
     }
 
     @Override
     public void unregisterController() {
-        Controller.super.unregisterController();
+        GuidanceController.super.unregisterController();
     }
 
     @Override
     public void injectStatusLabel(Label statusLabel) {
-        Controller.super.injectStatusLabel(statusLabel);
+        GuidanceController.super.injectStatusLabel(statusLabel);
     }
 
     @Override
@@ -40,14 +53,15 @@ public class GuidanceNavigationController implements Controller {
         registerController();
     }
 
-    public void onStopVisualizationClicked() {
-        guidanceHandler.switchToTab("GuidancePlanningView");
-    }
-
+    @Override
     public void setGuidanceHandler(GuidanceHandler guidanceHandler) {
         this.guidanceHandler = guidanceHandler;
         guidanceHandler.addGuidanceController(this);
         guidanceHandler.startNavigationLoop();
+    }
+
+    public void onStopVisualizationClicked() {
+        guidanceHandler.switchToTab("GuidancePlanningView");
     }
 
 
