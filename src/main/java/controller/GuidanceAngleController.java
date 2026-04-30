4275ff7cb51bea8b1346c4e1626ab455579656c2
lyro7 <lovro.lupis7@gmail.com>
2026-04-30 16:05:44 +0200

    Added new view and controller for the angle phase

---
+++ src/main/java/controller/GuidanceAngleController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GuidanceAngleController implements GuidanceController {
+
+    @FXML
+    public Circle targetCircle;
+
+    private GuidanceHandler guidanceHandler;
+
+    @Override
+    public void setGuidanceHandler(GuidanceHandler guidanceHandler) {
+        this.guidanceHandler = guidanceHandler;
+        guidanceHandler.addGuidanceController(this);
+    }
+
+    @Override
+    public void registerController() {
+        GuidanceController.super.registerController();
+    }
+
+    @Override
+    public void unregisterController() {
+        GuidanceController.super.unregisterController();
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void onStopVisualizationClicked(ActionEvent actionEvent) {
+        guidanceHandler.switchToTab("GuidancePlanningView");
+    }
+
+
+}
